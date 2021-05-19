8605f6cd817ec49cc46bb2a8fa189d714194f90d
Katelyn Rains <krains@tql.com>
2021-05-19 15:55:39 -0400

    Add project files.

---
+++ ClassTutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ ClassTutorial/TQLMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTutorial
+{
+    class TQLMath
+    {
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+    }
+}
